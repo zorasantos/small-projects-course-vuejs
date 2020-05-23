1b61df298f72ac4dc64d8a6064a8182a1237ca2a
zoranildosantos <zoranildo22@gmail.com>
2020-05-22 23:29:54 -0300

    Component stock

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,13 +1,45 @@
 <template>
-  <h1>Ação</h1>
+  <v-container>
+    <v-card>
+      <v-card-title :class="$style.cardTitleStyle" class="headline white--text">
+        <strong>{{ stock.name }} <small>Preço: {{ stock.price }}</small></strong>
+      </v-card-title>
+      <v-container fill-height>
+        <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
+        <v-btn
+          @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          class="green darken-3 white--text">Comprar
+        </v-btn>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  name: 'Stock',
+  props: ['stock'],
+  data: () => {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buyStock () {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      console.log(order)
+      this.quantity = 0
+    }
+  }
 }
 </script>
 
-<style>
-
+<style module>
+  .cardTitleStyle {
+    background-color: #3E8742;
+  }
 </style>
--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Acões</h1>
+  <v-container>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
+  </v-container>
 </template>
 
 <script>
+const Stock = () => import('@/components/stocks/Stock')
 export default {
+  name: 'Stocks',
+  components: { Stock },
+  data: () => {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110 },
+        { id: 2, name: 'Google', price: 200 },
+        { id: 3, name: 'Apple', price: 280 },
+        { id: 4, name: 'Twitter', price: 31 }
+      ]
+    }
+  }
 
 }
 </script>
